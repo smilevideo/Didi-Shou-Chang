77f1335bcc49e32a734cbea6a1a17d7a7e9d0f03
smilevideo <andrewk1216@gmail.com>
2020-11-27 17:38:37 -0500

    broadcast function for server

--- server/server.js
+++ server/server.js
@@ -2,14 +2,40 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 3030 });
 
+const broadcast = (ws, data, includeSelf = false) => {
+  if (includeSelf) {
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      }
+    );  
+  }
+  
+  else {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      }
+    );  
+  }
+
+  // think the below would sacrifice performance for less lines of code compared to above
+
+  // wss.clients.forEach((client) => {
+  //   if (client.readyState === WebSocket.OPEN) {
+  //     if (client !== ws || includeSelf) {
+  //       client.send(data);
+  //     }
+  //   }
+  // });
+}
+
 wss.on('connection', (ws) => {
   ws.on('message', (data) => {
     console.log(data);
 
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
+    broadcast(ws, data, false);
   });
 });
