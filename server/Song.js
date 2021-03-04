f99b82acfca13b277f7dab1415fcf5a1bb54c309
Pai Li <pl9ed15@gmail.com>
2021-03-04 01:45:49 -0500

    Added equals method to Song model

--- server/Song.js
+++ server/Song.js
@@ -10,4 +10,16 @@ export default class Song {
         this.label = label
         this.duration = duration
     }
+
+    equals(other) {
+        if (other instanceof Song) {
+            return (
+                this.username == other.username &&
+                this.url == other.url &&
+                this.label == other.label && // this might be a problem if label is also an object
+                this.duration == other.duration
+            )
+        }
+        return false
+    }
 }
