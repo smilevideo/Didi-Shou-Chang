17cd4fb378be15327a1d78837a3ed63f77e4f44d
smilevideo <andrewk1216@gmail.com>
2021-03-06 06:39:42 -0500

    refactors, remove unneeded users and lock fields

--- server/PriorityQ.js
+++ server/PriorityQ.js
@@ -5,171 +5,121 @@ Basic structure is a Map<String, Array>
 
 */
 export default class PriorityQ {
+  // JS map preserves insertion order of entries
+  qMap = new Map()
+  // array of qMap's keyset to iterate over by index
+  usernames = []
+  head = 0
+  length = 0 // length of overall queue
 
-    // JS map preserves insertion order of entries
-    qMap = new Map()
-    // array of qMap's keyset to iterate over by index
-    users = []
-    head = 0
-    length = 0 // length of overall queue
+  constructor() { }
 
-    // lock to make all of our operations atomic
-    inUse = false
-    // 
-    pollingRate = 50
+  // need to be careful with the timing of this method call to prevent
+  // index out of bounds or skipping when a user's final song has played
+  updateHead() {
+    if (this.head >= this.qMap.size - 1) {
+      this.head = 0
+    } else {
+      this.head++
+    }
+  }
 
-    constructor() { }
+  push(song) {
+    const username = song.username
 
-    push(song) {
-        if (!this.inUse) {
-            this.inUse = true
-            this._push(song)
-            this.inUse = false
-        } else {
-            setTimeout(() => {
-                this.push(song)
-            }, this.pollingRate)
-        }
+    if (this.qMap.has(username)) {
+      this.qMap.get(username).push(song)
+    } else {
+      this.qMap.set(username, [song])
     }
 
-    _push(song) {
-        let username = song.username
-        if (this.qMap.has(username)) {
-            // if username already exists in the map, 
-            // just push to existing array
-            this.qMap.get(username).push(song)
-        } else {
-            // otherwise, add new entry + update users
-            this.users.push(username)
-            this.qMap.set(username, [song])
-        }
-        this.length++
-    }
+    this.length++
+  }
 
-    shift() {
-        if (!this.inUse) {
-            this.inUse = true
-            let retVal = this._shift()
-            this.inUse = false
-            return retVal
-        } else {
-            setTimeout(() => {
-                return this.shift()
-            }, this.pollingRate)
+  shift() {
+    if (this.length > 0) {
+      const keysArray = Array.from(this.qMap.keys());
+      const username = keysArray[this.head];
+      const userSongList = this.qMap.get(username)
+      const nextSong = userSongList.shift()
+      
+      if (userSongList.length > 0) {
+        this.updateHead()
+      } else {
+        const toDeleteIndex =  keysArray.indexOf(username);
+        // if userSongList == 0 and user is last element of users
+        // we can just set head to 0 directly
+        // otherwise, don't update head, since we remove the user key
+        // and next user is at current head
+        if (toDeleteIndex == this.qMap.size - 1) {
+          this.head = 0
         }
+        this.qMap.delete(username)
+      }
+      this.length--
+      return nextSong
     }
+  }
 
-    _shift() {
-        if (this.length > 0) {
-            let username = this.users[this.head]
-            let userSongList = this.qMap.get(username)
-            let nextSong = userSongList.shift()
-            if (userSongList.length > 0) {
-                // if there are still elements, update head as usual
-                this.updateHead()
-            } else {
-                let toDeleteIndex = this.users.indexOf(username)
-                // if userSongList == 0 and user is last element of users
-                // we can just set head to 0 directly
-                // otherwise, don't update head, since we remove the user key
-                // and next user is at current head
-                if (toDeleteIndex == this.users.length - 1) {
-                    this.head = 0
-                }
-                this.qMap.delete(username)
-                this.users.splice(toDeleteIndex, 1)
+  // returns [username, index] of the element at index n
+  traverse(n) {
+    if (n >= 0 && n < this.length) {
+      let index = 0
+      let depth = 0
+      // logically, this while loop is for (depth < maxDepth), but that requires us to find maxDepth first
+      // instead we can just break if index !< n since that's what we really care about anyways
+      while (index <= n) {
+        // go through each song at user[depth] and check if it exists
+        // if it does, increment. if it doesn't, skip
+        for (const username of this.qMap.keys()) {
+          if (this.qMap.get(username)[depth]) {
+            if (index == n) {
+              return ({
+                username, 
+                depth 
+              });
             }
-            this.length--
-            return nextSong
+            index++
+          }
         }
+        // once we go through everyone, we increment depth and look through next "row"
+        depth++
+      }
     }
+  }
 
+  getSongAtIndex(i) {
+    const { username, depth } = this.traverse(i)
+    
+    return this.qMap.get(username)[depth];
+  }
 
-    getSongAtIndex(i) {
-        if (!this.inUse) {
-            this.inUse = true
-            let [username, depth] = this.traverse(i)
-            let retVal = this.qMap.get(username)[depth]
-            this.inUse = false
-            return retVal
-        } else {
-            setTimeout(() => {
-                return this.getSongAtIndex(i)
-            }, this.pollingRate)
+  removeSongAtIndex(i) {
+    const { username, depth } = this.traverse(i)
+    this.qMap.get(username).splice(depth, 1)
+    this.length--
+    // if we're removing the current song, then don't repeat turn
+    // go to next guy
+    if (i == 0) {
+      this.updateHead()
+    }
+  }
+
+  // literally just traverse, but we add elements into retArr
+  flatten() {
+    const retArr = []
+    let index = 0
+    let depth = 0
+    while (index < this.length) {
+      for (const username of this.qMap.keys()) {
+        let song = this.qMap.get(username)[depth]
+        if (song) {
+          retArr.push(song)
+          index++
         }
-
+      }
+      depth++
     }
-
-    removeSongAtIndex(i) {
-        if (!this.inUse) {
-            this.inUse = true
-            let [username, depth] = this.traverse(i)
-            this.qMap.get(username).splice(depth, 1)
-            this.length--
-            // if we're removing the current song, then don't repeat turn
-            // go to next guy
-            if (i == 0) {
-                this.updateHead()
-            }
-            this.inUse = false
-        } else {
-            setTimeout(() => {
-                this.removeSongAtIndex(i)
-            })
-        }
-    }
-
-    // returns [username, index] of the element at index n
-    traverse(n) {
-        if (n < this.length && n >= 0) {
-            let index = 0
-            let depth = 0
-            // logically, this while loop is for (depth < maxDepth), but that requires us to find maxDepth first
-            // instead we can just break if index !< n since that's what we really care about anyways
-            while (index <= n) {
-                // go through each song at user[depth] and check if it exists
-                // if it does, increment. if it doesn't, skip
-                for (const user of this.users) {
-                    if (this.qMap.get(user)[depth]) {
-                        if (index == n) {
-                            return [user, depth]
-                        }
-                        index++
-                    }
-                }
-                // once we go through everyone, we increment depth and look through next "row"
-                depth++
-            }
-        }
-    }
-
-    // literally just traverse, but we add elements into retArr
-    flatten() {
-        let retArr = []
-        let index = 0
-        let depth = 0
-        while (index < this.length) {
-            for (const user of this.users) {
-                let song = this.qMap.get(user)[depth]
-                if (song) {
-                    retArr.push(song)
-                    index++
-                }
-            }
-            depth++
-        }
-        return retArr
-    }
-
-    // need to be careful with the timing of this method call to prevent
-    // index out of bounds or skipping when a user's final song has played
-    updateHead() {
-        // want to reset head if it's >= index of last element
-        if (this.head >= this.users.length - 1) {
-            this.head = 0
-        } else {
-            this.head++
-        }
-    }
-
+    return retArr
+  }
 }
