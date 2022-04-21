c9ebc2fe342312883768fcd85ee84cb9d61ea4d2
Pai Li <pl9ed15@gmail.com>
2022-04-21 19:12:44 -0400

    Removed unused classes

--- s3/src/main/java/tubalubback/utils/SyncUtils.java
+++
@@ -1,7 +0,0 @@
-package tubalubback.utils;
-
-public class SyncUtils {
-
-    public static final String SYNC_STRING = "SYNC_STRING";
-    public static final String USER_WS_DESTINATION = "/topic/users";
-}
