3bd59473d11f06f354179180a6f5f331d06373e5
Pai Li <pl9ed15@gmail.com>
2022-04-22 02:03:27 -0400

    Added /bin to cmds in userdata script, removed redundant echos in startup.sh

--- cicd/scripts/startup.sh
+++ cicd/scripts/startup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # script to be executed on EC2 instance start up/reboot
-echo "Starting up Jenkins job"
-echo "Using API Token starting with: $(echo $JENKINS_API_TOKEN | cut -c 1-4)"
+# https://aws.amazon.com/premiumsupport/knowledge-center/execute-user-data-ec2/
+
+
 PUBLIC_IP=$(curl http://checkip.amazonaws.com | xargs)
-echo "EC2 public IP: $PUBLIC_IP"
 
 sudo curl -X POST \
     -u didishouchang:"$JENKINS_API_TOKEN" \
     --max-time 30 \
     "http://localhost:8080/job/ec2_startup/buildWithParameters?BRANCH=main&TRIGGER_DOWNSTREAM_JOBS=true"
 
-touch $HOME/startup_script_ran
+touch /home/ec2-user/startup_script_ran
