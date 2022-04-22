d4fb9cdaa7d0a6a4459ce584f47dc7ccc0ea3d92
Pai Li <pl9ed15@gmail.com>
2022-04-22 02:06:21 -0400

    Added echo back to startup.sh

--- cicd/scripts/startup.sh
+++ cicd/scripts/startup.sh
@@ -3,8 +3,10 @@
 # script to be executed on EC2 instance start up/reboot
 # https://aws.amazon.com/premiumsupport/knowledge-center/execute-user-data-ec2/
 
-
+echo "Starting up Jenkins job"
+echo "Using API Token starting with: $(echo $JENKINS_API_TOKEN | cut -c 1-4)"
 PUBLIC_IP=$(curl http://checkip.amazonaws.com | xargs)
+echo "EC2 public IP: $PUBLIC_IP"
 
 sudo curl -X POST \
     -u didishouchang:"$JENKINS_API_TOKEN" \
