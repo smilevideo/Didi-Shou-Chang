fd1f46ed66020695eceb319d46ff18063e7c68d4
Pai <pl9ed15@gmail.com>
2022-04-22 03:21:29 -0400

    Added EC2 userdata + minor adjustments to startup script (#50)
    * Added userdata file
    
    * Added /bin to cmds in userdata script
    
    * Explicitly source bashrc + clean up workspace

--- cicd/scripts/startup.sh
+++ cicd/scripts/startup.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # script to be executed on EC2 instance start up/reboot
-echo "Starting up Jenkins job"
-echo "Using API Token starting with: $(echo $JENKINS_API_TOKEN | cut -c 1-4)"
+# https://aws.amazon.com/premiumsupport/knowledge-center/execute-user-data-ec2/
+
+# need to source directly since this script runs on startup of instance
+# meaning it's early enough in lifecycle that we don't have our typical environment
+source /home/ec2-user/.bashrc
+
+# clean jenkins workspace + logs
+rm -rf /var/lib/jenkins/workspace/*
+rm -rf /var/log/jenkins/*
+
 PUBLIC_IP=$(curl http://checkip.amazonaws.com | xargs)
-echo "EC2 public IP: $PUBLIC_IP"
+echo "PUBLIC_IP: $PUBLIC_IP, JENKINS_API_TOKEN: $(echo $JENKINS_API_TOKEN | cut -c 1-4)" >> /home/ec2-user/startup_var.txt
 
 sudo curl -X POST \
     -u didishouchang:"$JENKINS_API_TOKEN" \
     --max-time 30 \
-    "http://localhost:8080/job/ec2_startup/buildWithParameters?BRANCH=main&TRIGGER_DOWNSTREAM_JOBS=true"
+    "http://localhost:8080/job/ec2_startup/buildWithParameters?BRANCH=main&TRIGGER_DOWNSTREAM_JOBS=true" >> /home/ec2-user/startup_request.log
